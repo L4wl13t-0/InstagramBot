f693c8c3ef7fe2c4be03ef4e91e2e45d3c3c356d
Darwin Felipe Uzcategui Portatil Lenovo <darwin.uzcategui1973@hotmail.com>
2020-05-05 16:28:40 -0400

    cambio en story_by_user1 realiza un cliclo de acuerdo al numero de uauario existente

--- Bots/Story_By_User1.py
+++ Bots/Story_By_User1.py
@@ -32,7 +32,7 @@ def get_users():
             "insta_password": dat["password"]
         })
     # print(datos)
-    return datos[0]
+    return datos
 
 
 # Creamos las variables que guarda la cuenta
@@ -50,27 +50,29 @@ print(story_by_users)
 # insta_username = 'jokael_nimerrs'
 # insta_password = 'houkai'
 
+
 usuarios = get_users()
-insta_username = usuarios['insta_username']  # 'darwinjosejosepedro'
-insta_password = usuarios['insta_password']  # 'Clave321**'
+for unUser in usuarios:
 
+    insta_username = unUser['insta_username']  # 'darwinjosejosepedro'
+    insta_password = unUser['insta_password']  # 'Clave321**'
 
-# creamos la variable donde se almacena e inicia la cuenta
-session = InstaPy(username=insta_username,
-                  password=insta_password,
-                  headless_browser=False)
+    # creamos la variable donde se almacena e inicia la cuenta
+    session = InstaPy(username=insta_username,
+                      password=insta_password,
+                      headless_browser=False)
 
-# Definicmos la funcion que inicia la cuenta
-with smart_run(session):
+    # Definicmos la funcion que inicia la cuenta
+    with smart_run(session):
 
-    # Damos los parametros de la cuenta
-    session.set_relationship_bounds(enabled=True,
-                                    delimit_by_numbers=True,
-                                    max_followers=4000,
-                                    min_followers=2,
-                                    min_following=2)
+        # Damos los parametros de la cuenta
+        session.set_relationship_bounds(enabled=True,
+                                        delimit_by_numbers=True,
+                                        max_followers=4000,
+                                        min_followers=2,
+                                        min_following=2)
 
-    session.set_do_story(enabled=True, percentage=95, simulate=True)
+        session.set_do_story(enabled=True, percentage=95, simulate=True)
 
-    session.story_by_users(story_by_users)
-    # session.story_by_users(['cristiano', 'jumpman23', 'noah_bellhat', "david_braham00"])
+        # session.story_by_users(story_by_users)
+        session.story_by_users(["darwinjosejosepedro"])
